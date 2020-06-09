9246b0e845721f394c6fb0868af964cef0d28960
Brindhaa-kaleeswaran <66661993+Brindhaa-kaleeswaran@users.noreply.github.com>
2020-06-09 21:31:30 +0530

    Create source.py

---
+++ source.py
@@ -0,0 +1,34 @@
+Def listt():
+         # empty list
+          my_list = []
+
+          # list of integers
+          my_list = [1, 2, 3]
+
+           # list with mixed data types
+             my_list = [1, "Hello", 3.4]
+
+Def tuple():
+
+            my_tuple = ('p','r','o','g','r','a','m','i','z')
+
+     print(my_tuple[1:4])
+
+      print(my_tuple[:-7])
+
+      print(my_tuple[7:])
+
+      print(my_tuple[:])
+Def dict():
+       squares = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+     print(squares.pop(4))
+     print(squares)
+  print(squares.popitem())
+
+# Output: {1: 1, 2: 4, 3: 9}
+      print(squares)
+      squares.clear()
+
+      print(squares)
+      print(squares)
